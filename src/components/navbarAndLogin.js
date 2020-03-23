9f1e5210de977adc0089b43eda38916f87f43ea8
Boo1122 <you@example.com>
2020-03-23 10:14:08 +0200

    task 1 done

--- src/components/navbarAndLogin.js
+++ src/components/navbarAndLogin.js
@@ -1,37 +1,31 @@
+import {navigate} from '../components/navigation/navigateHistory'
+
 export class Navbar {
     constructor() {
         this.nav();
     }
 
     nav() {
-      let body = document.getElementById('body')
-      let containerDiv = document.createElement('div')
-      containerDiv.classList.add('container-fluid', 'px-0')
+        let body = document.getElementById('body')
+        let containerDiv = document.createElement('div')
+        containerDiv.classList.add('container-fluid', 'px-0')
+
+        body.appendChild(containerDiv)
+
+        let navbar = document.createElement('nav');
+        navbar.classList.add('navbar', 'navbar-light', 'justify-content-end');
+        containerDiv.appendChild(navbar)
+
+        let anchor = document.createElement('a');
+        anchor.classList.add('navbar-brand', 'active', 'nav-link');
+        anchor.setAttribute('data-target', 'login')
+        anchor.href = '#'
+        anchor.id = 'login'
+        anchor.innerHTML = 'Login'
+        anchor.style.color = 'black';
         
-      body.appendChild(containerDiv)
 
-      let navbar = document.createElement('nav');
-      navbar.classList.add('navbar', 'navbar-light', 'justify-content-end');
-      containerDiv.appendChild(navbar)
-
-      let anchor = document.createElement('a');
-      anchor.classList.add('navbar-brand');
-      anchor.href = '#'
-      anchor.innerHTML = 'Login'
-      anchor.style.color = 'black';
-      
-
-    //   let p = document.createElement('p')
-    //     p.innerHTML = 'Nav'
-       
-
-    //     anchor.appendChild(p)
-
-      
-
-      navbar.appendChild(anchor);
-
-   
+        navbar.appendChild(anchor);
     }
 
 
@@ -43,5 +37,3 @@ export class Navbar {
 }
 
 const newNevbar = new Navbar();
-
-
