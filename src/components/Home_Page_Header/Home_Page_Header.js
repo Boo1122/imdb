1ac1e514e761f56f9b15608f6e92b4f34a0c84a1
George4888 <DanGeorge4888@gmail.com>
2020-03-25 18:21:18 +0200

    fix 2

--- src/components/Home_Page_Header/Home_Page_Header.js
+++ src/components/Home_Page_Header/Home_Page_Header.js
@@ -1,29 +1,28 @@
 // import {navigate} from './components/Navigate_History/Navigate_History';
 
 export class Header {
-    constructor() {
-        this.Header();
-    }
+  constructor() {
+    this.Header();
+  }
 
-    Header() {
-        let body = document.getElementById('body')
-      
-        let header = document.createElement('div');
-        header.classList.add('header');
-        body.appendChild(header)
+  Header() {
+    const body = document.getElementById("body");
 
-        let anchor = document.createElement('a');
-        anchor.classList.add('active', 'nav-link');
-        anchor.setAttribute('data-target', 'login')
-        anchor.id = 'login'
-        anchor.innerHTML = 'Login'
-        
-        
-        header.appendChild(anchor);
-    }
+    const header = document.createElement("div");
+    header.classList.add("header");
+    body.appendChild(header);
 
+    const movies = document.createElement("button");
+    movies.classList.add("movies-btn");
+    movies.innerText = "Movies";
 
+    const anchor = document.createElement("a");
+    anchor.classList.add("active", "nav-link");
+    anchor.setAttribute("data-target", "login");
+    anchor.id = "login";
+    anchor.innerHTML = "Login";
+
+    header.appendChild(movies);
+    header.appendChild(anchor);
+  }
 }
-
-
-
