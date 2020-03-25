0914dbf9c73e453bb31087147fc1a1e2f9982673
George4888 <DanGeorge4888@gmail.com>
2020-03-25 16:07:41 +0200

    fix pre-production

--- src/components/Home_Page_Header/Home_Page_Header_Script.js
+++ src/components/Home_Page_Header/Home_Page_Header_Script.js
@@ -1,5 +1,5 @@
-export function header () {
-    const headerContainer = document.createElement('div');
-    document.body.appendChild(headerContainer);
-    headerContainer.id= 'headerContainer'
+export function header() {
+  const headerContainer = document.createElement("div");
+  document.body.appendChild(headerContainer);
+  headerContainer.id = "headerContainer";
 }
