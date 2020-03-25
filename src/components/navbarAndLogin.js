c264066fba60735969238c687e620b2be6cc8432
Boo1122 <you@example.com>
2020-03-25 15:16:18 +0200

    no bootstrap task 1

--- src/components/navbarAndLogin.js
+++ src/components/navbarAndLogin.js
@@ -2,22 +2,18 @@ import {navigate} from '../components/navigation/navigateHistory'
 
 export class Navbar {
     constructor() {
-        this.nav();
+        this.Nav();
     }
 
-    nav() {
+    Nav() {
         let body = document.getElementById('body')
-        let containerDiv = document.createElement('div')
-        containerDiv.classList.add('container-fluid', 'px-0')
-
-        body.appendChild(containerDiv)
-
-        let navbar = document.createElement('nav');
-        navbar.classList.add('navbar', 'navbar-light', 'justify-content-end');
-        containerDiv.appendChild(navbar)
+      
+        let header = document.createElement('div');
+        header.classList.add('header');
+        body.appendChild(header)
 
         let anchor = document.createElement('a');
-        anchor.classList.add('navbar-brand', 'active', 'nav-link');
+        anchor.classList.add('active', 'nav-link');
         anchor.setAttribute('data-target', 'login')
         anchor.href = '#'
         anchor.id = 'login'
@@ -25,7 +21,7 @@ export class Navbar {
         anchor.style.color = 'black';
         
 
-        navbar.appendChild(anchor);
+        header.appendChild(anchor);
     }
 
 
