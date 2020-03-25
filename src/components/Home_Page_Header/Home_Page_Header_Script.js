a443a3ac0329c963bbda7c73c04e3815379f8c95
Boo1122 <you@example.com>
2020-03-25 17:03:56 +0200

    task 1

--- src/components/Home_Page_Header/Home_Page_Header_Script.js
+++
@@ -1,5 +0,0 @@
-export function header() {
-  const headerContainer = document.createElement("div");
-  document.body.appendChild(headerContainer);
-  headerContainer.id = "headerContainer";
-}
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-import { header } from "./components/Home_Page_Header/Home_Page_Header_Script";
-
-header();
-
 console.log("start");
+
+import { Header } from "./components/Home_Page_Header/Home_Page_Header"
+
+let newHeader = new Header();
