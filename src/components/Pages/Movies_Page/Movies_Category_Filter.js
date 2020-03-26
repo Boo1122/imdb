b6ead815e1dd34265961253c0bb3964223536cda
George4888 <DanGeorge4888@gmail.com>
2020-03-26 17:42:10 +0200

    testing

--- src/components/Pages/Movies_Page/Movies_Category_Filter.js
+++ src/components/Pages/Movies_Page/Movies_Category_Filter.js
@@ -1,18 +1,31 @@
 export class MoviesCategoryFilter {
   constructor(text) {
     this.cat = text;
-    this.Title(text);
+    this.Name(text);
   }
 
-  Title(text) {
+  Name(text) {
     const movieBtn = document.getElementById("movBtn");
 
     const div = document.createElement("div");
     div.id = `${text}-div`;
     const p = document.createElement("p");
+    p.classList.add("allMovieButtons");
     p.innerText = `${text}`;
 
     movieBtn.appendChild(div);
     div.appendChild(p);
   }
+}
+
+export function moviesClicked() {
+  const movieBtn = document.getElementById("movBtn");
+
+  movieBtn.addEventListener("click", event => {
+    event.preventDefault();
+    console.log("clicked");
+    const title = new MoviesCategoryFilter(Title);
+    const year = new MoviesCategoryFilter(Year);
+    return title, year;
+  });
 }
--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-console.log("start");
-
 import { Header } from "./components/Home_Page_Header/Home_Page_Header";
 import { MoviesCategoryFilter } from "./components/Pages/Movies_Page/Movies_Category_Filter";
+import { moviesClicked } from "./components/Pages/Movies_Page/Movies_Category_Filter";
 
 new Header();
 
-new MoviesCategoryFilter("Title");
-new MoviesCategoryFilter("Year");
-new MoviesCategoryFilter("Runtime");
-new MoviesCategoryFilter("Genre");
+moviesClicked();
