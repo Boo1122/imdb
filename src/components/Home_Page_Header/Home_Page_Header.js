8d7386b2fee7b8a6b4bd3eed092cf31df9de6f8a
George4888 <DanGeorge4888@gmail.com>
2020-03-27 18:59:02 +0200

    small delete

--- src/components/Home_Page_Header/Home_Page_Header.js
+++ src/components/Home_Page_Header/Home_Page_Header.js
@@ -1,5 +1,4 @@
 // import {navigate} from './components/Navigate_History/Navigate_History';
-import { MoviesCategoryFilter } from "../Pages/Movies_Page/Movies_Category_Filter";
 
 export class Header {
   constructor() {
