e21ba41f17d5e361f905c968f9a1fdbafd68130a
Boo1122 <you@example.com>
2020-03-29 11:44:04 +0300

    task done search bar

--- src/components/Home_Page_Header/Search_Button.js
+++ src/components/Home_Page_Header/Search_Button.js
@@ -9,19 +9,14 @@ export class Search {
         let inputDiv = document.createElement("div");
         inputDiv.className = "input";
         inputDiv.id = "input-div";
-        // inputDiv.classList.add('search-box')
+       
 
         let input = document.createElement("input");
         input.setAttribute("type", "text");
         input.id = "input";
-        input.classList.add('search-txt');
         input.placeholder = "Type to search";
 
-        let a = document.createElement('a')
-        a.classList.add('search-btn')
-
-
-        input.appendChild(a)
+     
         header.appendChild(inputDiv);
         inputDiv.appendChild(input);
         header.appendChild(img);
