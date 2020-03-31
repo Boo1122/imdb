feb35c805ea4e46834fe9707c6917b03c0e6c2f1
George4888 <dangeorge4888@gmail.com>
2020-03-31 17:33:26 +0300

    COMPLETE LOGIN BUTTON 100%

--- src/components/Pages/Login_Form/Switch_Login_Register.js
+++ src/components/Pages/Login_Form/Switch_Login_Register.js
@@ -1,35 +1,30 @@
-
-
 export class Switch {
-    constructor() {
-        this.Login();
-        this.Register();
-    }
+  constructor() {
+    this.Login();
+    this.Register();
+  }
 
+  Login() {
+    const login = document.getElementById("log-button");
+    const logPage = document.getElementById("login-container");
+    const regPage = document.getElementById("form-container");
 
-    Login() {
-        const login = document.getElementById('log-button')
-        const logPage = document.getElementById('login-container')
-        const regPage = document.getElementById('form-container')
+    login.addEventListener("click", event => {
+      event.preventDefault();
+      logPage.style.display = "flex";
+      regPage.style.display = "none";
+    });
+  }
 
-        login.addEventListener('click', event => {
-            event.preventDefault();
-            console.log(event.target.id)
-            logPage.style.display = 'flex'
-        })
+  Register() {
+    const register = document.getElementById("reg-button");
+    const logPage = document.getElementById("login-container");
+    const regPage = document.getElementById("form-container");
 
-    }
-
-    Register() {
-        const register = document.getElementById('reg-button')
-        const logPage = document.getElementById('login-container')
-        const regPage = document.getElementById('form-container')
-
-        register.addEventListener('click', event => {
-            event.preventDefault();
-            console.log(event.target.id)
-            regPage.style.display = 'flex'
-        })
-
-    }
+    register.addEventListener("click", event => {
+      event.preventDefault();
+      regPage.style.display = "flex";
+      logPage.style.display = "none";
+    });
+  }
 }
