5af2f3c4c52471cd02e88251116506ba3384b5ee
iCosteaa <i.ioana3133@yahoo.com>
2020-04-10 23:57:23 +0300

    bug primul username

--- src/components/Home_Page_Main/Caroussel/Caroussel.js
+++ src/components/Home_Page_Main/Caroussel/Caroussel.js
@@ -6,8 +6,6 @@ export class Caroussel {
     this.slides = [];
     this.current = 0;
     this.next;
-    //this.nextSlide();
-    //this.prevSlide();
     this.carousel();
     this.leftButton();
     this.rightButton();
@@ -30,7 +28,7 @@ export class Caroussel {
 
     const carousselTitleDiv = document.createElement("h1");
     const carousselTitle = document.createTextNode(
-      "This is why our users love us!"
+      "THIS IS WHY OUR USERS LOVE US!"
     );
     carousselTitleDiv.id = "carousselTitleDivID";
     carousselTitleDiv.classList.add("carousselTitleDivClass");
@@ -40,7 +38,7 @@ export class Caroussel {
     const prevBot = document.createElement("div");
     prevTop.classList.add("arrow-top");
     prevBot.classList.add("arrow-bottom");
-    prev.classList.add("prev");
+    prev.classList.add("prevBtn");
     prev.id = "prev";
 
     const next = document.createElement("div");
@@ -48,7 +46,7 @@ export class Caroussel {
     const nextBot = document.createElement("div");
     nextTop.classList.add("arrow-top2");
     nextBot.classList.add("arrow-bottom2");
-    next.classList.add("next");
+    next.classList.add("nextBtn");
     next.id = "next";
 
     const innerDiv1 = document.createElement("div");
@@ -60,14 +58,23 @@ export class Caroussel {
     icon1.classList.add("userIcon");
     icon1.id = "icon1";
     const img1 = document.createElement("img");
+    img1.classList.add("iconImg");
     img1.setAttribute("src", "../icons/avatar1.png");
     icon1.appendChild(img1);
+
+    const usDiv1 = document.createElement("div");
+    usDiv1.id = "usDiv1";
+    usDiv1.classList.add("usDiv");
+
+    const username1 = document.createElement("p");
+    username1.innerText = "Frida Kahlo";
+    username1.classList.add("username");
 
     const p1 = document.createElement("p");
     p1.classList.add("review");
     p1.id = "p1";
     p1.innerText =
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ";
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
     const innerDiv2 = document.createElement("div");
     innerDiv2.id = "innerDivID2";
@@ -76,6 +83,18 @@ export class Caroussel {
     const icon2 = document.createElement("div");
     icon2.classList.add("userIcon");
     icon2.id = "icon2";
+    const img2 = document.createElement("img");
+    img2.classList.add("iconImg");
+    img2.setAttribute("src", "../icons/avatar2.png");
+    icon2.appendChild(img2);
+
+    const usDiv2 = document.createElement("div");
+    usDiv2.id = "usDiv2";
+    usDiv2.classList.add("usDiv");
+
+    const username2 = document.createElement("p");
+    username2.innerText = "Margaret Thatcher";
+    username2.classList.add("username");
 
     const p2 = document.createElement("p");
     p2.classList.add("review");
@@ -90,6 +109,18 @@ export class Caroussel {
     const icon3 = document.createElement("div");
     icon3.classList.add("userIcon");
     icon3.id = "icon3";
+    const img3 = document.createElement("img");
+    img3.classList.add("iconImg");
+    img3.setAttribute("src", "../icons/avatar3.png");
+    icon3.appendChild(img3);
+
+    const usDiv3 = document.createElement("div");
+    usDiv3.id = "usDiv3";
+    usDiv3.classList.add("usDiv");
+
+    const username3 = document.createElement("p");
+    username3.innerText = "Martin Luther King";
+    username3.classList.add("username");
 
     const p3 = document.createElement("p");
     p3.classList.add("review");
@@ -104,6 +135,18 @@ export class Caroussel {
     const icon4 = document.createElement("div");
     icon4.classList.add("userIcon");
     icon4.id = "icon4";
+    const img4 = document.createElement("img");
+    img4.classList.add("iconImg");
+    img4.setAttribute("src", "../icons/avatar4.png");
+    icon4.appendChild(img4);
+
+    const usDiv4 = document.createElement("div");
+    usDiv4.id = "usDiv4";
+    usDiv4.classList.add("usDiv");
+
+    const username4 = document.createElement("p");
+    username4.innerText = "Friedrich Nietzsche";
+    username4.classList.add("username");
 
     const p4 = document.createElement("p");
     p4.classList.add("review");
@@ -118,6 +161,18 @@ export class Caroussel {
     const icon5 = document.createElement("div");
     icon5.classList.add("userIcon");
     icon5.id = "icon5";
+    const img5 = document.createElement("img");
+    img5.classList.add("iconImg");
+    img5.setAttribute("src", "../icons/avatar5.png");
+    icon5.appendChild(img5);
+
+    const usDiv5 = document.createElement("div");
+    usDiv5.id = "usDiv5";
+    usDiv5.classList.add("usDiv");
+
+    const username5 = document.createElement("p");
+    username5.innerText = "Nadia Comaneci";
+    username5.classList.add("username");
 
     const p5 = document.createElement("p");
     p5.classList.add("review");
@@ -132,6 +187,18 @@ export class Caroussel {
     const icon6 = document.createElement("div");
     icon6.classList.add("userIcon");
     icon6.id = "icon6";
+    const img6 = document.createElement("img");
+    img6.classList.add("iconImg");
+    img6.setAttribute("src", "../icons/avatar6.png");
+    icon6.appendChild(img6);
+
+    const usDiv6 = document.createElement("div");
+    usDiv6.id = "usDiv6";
+    usDiv6.classList.add("usDiv");
+
+    const username6 = document.createElement("p");
+    username6.innerText = "George Enescu";
+    username6.classList.add("username");
 
     const p6 = document.createElement("p");
     p6.classList.add("review");
@@ -146,6 +213,18 @@ export class Caroussel {
     const icon7 = document.createElement("div");
     icon7.classList.add("userIcon");
     icon7.id = "icon7";
+    const img7 = document.createElement("img");
+    img7.classList.add("iconImg");
+    img7.setAttribute("src", "../icons/avatar7.png");
+    icon7.appendChild(img7);
+
+    const usDiv7 = document.createElement("div");
+    usDiv7.id = "usDiv7";
+    usDiv7.classList.add("usDiv");
+
+    const username7 = document.createElement("p");
+    username7.innerText = "Stephen Hawking";
+    username7.classList.add("username");
 
     const p7 = document.createElement("p");
     p7.classList.add("review");
@@ -166,24 +245,44 @@ export class Caroussel {
 
     reviewsContainer.appendChild(innerDiv1);
     innerDiv1.appendChild(icon1);
+    innerDiv1.appendChild(usDiv1);
+    usDiv1.appendChild(username1);
     innerDiv1.appendChild(p1);
+
     reviewsContainer.appendChild(innerDiv2);
     innerDiv2.appendChild(icon2);
+    innerDiv2.appendChild(usDiv2);
+    usDiv2.appendChild(username2);
     innerDiv2.appendChild(p2);
+
     reviewsContainer.appendChild(innerDiv3);
     innerDiv3.appendChild(icon3);
+    innerDiv3.appendChild(usDiv3);
+    usDiv3.appendChild(username3);
     innerDiv3.appendChild(p3);
+
     reviewsContainer.appendChild(innerDiv4);
     innerDiv4.appendChild(icon4);
+    innerDiv4.appendChild(usDiv4);
+    usDiv4.appendChild(username4);
     innerDiv4.appendChild(p4);
+
     reviewsContainer.appendChild(innerDiv5);
     innerDiv5.appendChild(icon5);
+    innerDiv5.appendChild(usDiv5);
+    usDiv5.appendChild(username5);
     innerDiv5.appendChild(p5);
+
     reviewsContainer.appendChild(innerDiv6);
     innerDiv6.appendChild(icon6);
+    innerDiv6.appendChild(usDiv6);
+    usDiv6.appendChild(username6);
     innerDiv6.appendChild(p6);
+
     reviewsContainer.appendChild(innerDiv7);
     innerDiv7.appendChild(icon7);
+    innerDiv7.appendChild(usDiv1);
+    usDiv7.appendChild(username7);
     innerDiv7.appendChild(p7);
 
     outsideCaroussel.appendChild(next);
