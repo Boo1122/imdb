48fee86145fb7fe785756ec08da33507f10b0237
Boo1122 <you@example.com>
2020-04-10 12:24:43 +0300

    pagination colors

--- src/components/Pages/Movie_Page/Movie_Page.js
+++ src/components/Pages/Movie_Page/Movie_Page.js
@@ -94,63 +94,87 @@ export class MoviePage {
     next.className = "next";
     next.innerText = `Next >`;
 
+    const pagesContainer = document.createElement('div')
+    pagesContainer.id = 'pages-container'
     const page1 = document.createElement("p");
     page1.innerText = "1";
     page1.classList.add("nav-link", "pageNumber");
-    page1.setAttribute("data-target", ``); //"movie-page1" intre ghilimele
+    page1.setAttribute("data-target", 'next-movie'); //"movie-page1" intre ghilimele
 
     const page2 = document.createElement("p");
     page2.innerText = "2";
     page2.classList.add("nav-link", "pageNumber");
+    page2.setAttribute("data-target", 'next-movie')
+  
 
     const page3 = document.createElement("p");
     page3.innerText = "3";
     page3.classList.add("nav-link", "pageNumber");
+    page3.setAttribute("data-target", 'next-movie')
 
     const page4 = document.createElement("p");
     page4.innerText = "4";
     page4.classList.add("nav-link", "pageNumber");
+    page4.setAttribute("data-target", 'next-movie')
 
     const page5 = document.createElement("p");
     page5.innerText = "5";
     page5.classList.add("nav-link", "pageNumber");
+    page5.setAttribute("data-target", 'next-movie')
 
     const page6 = document.createElement("p");
     page6.innerText = "6";
     page6.classList.add("nav-link", "pageNumber");
+    page6.setAttribute("data-target", 'next-movie')
 
     const page7 = document.createElement("p");
     page7.innerText = "7";
     page7.classList.add("nav-link", "pageNumber");
+    page7.setAttribute("data-target", 'next-movie')
 
     const page8 = document.createElement("p");
     page8.innerText = "8";
     page8.classList.add("nav-link", "pageNumber");
+    page8.setAttribute("data-target", 'next-movie')
 
     const page9 = document.createElement("p");
     page9.innerText = "9";
     page9.classList.add("nav-link", "pageNumber");
+    page9.setAttribute("data-target", 'next-movie')
 
     const page10 = document.createElement("p");
     page10.innerText = "10";
     page10.classList.add("nav-link", "pageNumber");
+    page10.setAttribute("data-target", 'next-movie')
 
     body.appendChild(paginationMovie);
     paginationMovie.appendChild(paginationDiv);
-    paginationDiv.appendChild(previous);
+    paginationDiv.appendChild(pagesContainer);
+    pagesContainer.appendChild(previous);
 
-    paginationDiv.appendChild(page1);
-    paginationDiv.appendChild(page2);
-    paginationDiv.appendChild(page3);
-    paginationDiv.appendChild(page4);
-    paginationDiv.appendChild(page5);
-    paginationDiv.appendChild(page6);
-    paginationDiv.appendChild(page7);
-    paginationDiv.appendChild(page8);
-    paginationDiv.appendChild(page9);
-    paginationDiv.appendChild(page10);
+    pagesContainer.appendChild(page1)
+    pagesContainer.appendChild(page2)
+    pagesContainer.appendChild(page3)
+    pagesContainer.appendChild(page4)
+    pagesContainer.appendChild(page5)
+    pagesContainer.appendChild(page6)
+    pagesContainer.appendChild(page7)
+    pagesContainer.appendChild(page8)
+    pagesContainer.appendChild(page9)
+    pagesContainer.appendChild(page10)
 
-    paginationDiv.appendChild(next);
+    // paginationDiv.appendChild(page1);
+    // paginationDiv.appendChild(page2);
+    // paginationDiv.appendChild(page3);
+    // paginationDiv.appendChild(page4);
+    // paginationDiv.appendChild(page5);
+    // paginationDiv.appendChild(page6);
+    // paginationDiv.appendChild(page7);
+    // paginationDiv.appendChild(page8);
+    // paginationDiv.appendChild(page9);
+    // paginationDiv.appendChild(page10);
+
+    pagesContainer.appendChild(next);
   }
 
   paginationFetch(
@@ -168,7 +192,7 @@ export class MoviePage {
             .json()
             .then((moviesData) => {
               movies = movies.concat(moviesData);
-              console.log(moviesData.pagination.links.next);
+              // console.log(moviesData.pagination.links.next);
               if (moviesData.pagination.links.next) {
                 progress && progress(movies);
                 this.paginationFetch(
