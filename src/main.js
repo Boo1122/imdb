e6c871333356ad6c397f8a02f5e1e89a888ecd8f
Boo1122 <you@example.com>
2020-04-13 21:27:24 +0300

    onsave

--- src/main.js
+++ src/main.js
@@ -1,13 +1,31 @@
-import { Header } from "./components/Home_Page_Header/Home_Page_Header";
-import { RegisterPage } from "./components/Pages/Register_Page/Register_Page";
-import { LoginPage } from "./components/Pages/Login_Page/Login_Page";
-import { HomePage } from "./components/Pages/Home_Page/Home_Page";
+import {
+    Header
+} from "./components/Home_Page_Header/Home_Page_Header";
+import {
+    RegisterPage
+} from "./components/Pages/Register_Page/Register_Page";
+import {
+    LoginPage
+} from "./components/Pages/Login_Page/Login_Page";
+import {
+    HomePage
+} from "./components/Pages/Home_Page/Home_Page";
 import "./main.css";
-import { Footer } from "./components/Footer/Footer";
-import { navigate } from "./components/Navigate_History/Navigate_History";
-import { SinglePage } from "./components/Pages/Single_Page/Single_Page";
-import { MoviePage } from "./components/Pages/Movie_Page/Movie_Page";
-import { LogOutPage } from "./components/Pages/LogOut_Page/LogOut_Page";
+import {
+    Footer
+} from "./components/Footer/Footer";
+import {
+    navigate
+} from "./components/Navigate_History/Navigate_History";
+import {
+    SinglePage
+} from "./components/Pages/Single_Page/Single_Page";
+import {
+    MoviePage
+} from "./components/Pages/Movie_Page/Movie_Page";
+import {
+    LogOutPage
+} from "./components/Pages/LogOut_Page/LogOut_Page";
 
 new Header();
 new HomePage();
