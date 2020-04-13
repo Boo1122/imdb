5027357fae4a2f67a787527f14220e7075a12f16
Boo1122 <you@example.com>
2020-04-13 22:10:13 +0300

    trying

--- src/components/Pages/Movie_Page/Movie_Page.js
+++ src/components/Pages/Movie_Page/Movie_Page.js
@@ -1,5 +1,7 @@
 import "./Movie_Page.css";
-import { navigate } from "../../Navigate_History/Navigate_History";
+import {
+  navigate
+} from "../../Navigate_History/Navigate_History";
 
 export class MoviePage {
   constructor() {
@@ -87,6 +89,10 @@ export class MoviePage {
     previous.classList.add("nav-link");
     previous.className = "previous";
     previous.innerText = `< Previous`;
+    previous.addEventListener("click", () => {
+      this.getMovies(this.movieData.pagination.currentPage / 10);
+    });
+
 
     const next = document.createElement("button");
     next.id = "next-movie";
@@ -97,70 +103,31 @@ export class MoviePage {
       this.getMovies(this.movieData.pagination.currentPage * 10);
     });
 
-    const pagesContainer = document.createElement("div");
-    pagesContainer.id = "pages-container";
-    const page1 = document.createElement("p");
-    page1.innerText = "1";
 
-    const page2 = document.createElement("p");
-    page2.innerText = "2";
+    let store = [];
+    let pagesContainer = document.createElement("div");
+    pagesContainer.id = 'pages-container'
+    let page = document.createElement("p");
+    // page.innerText = `${}`
+    for (let i = 0; i <= paginationMovie.length; i++) {
+      page.addEventListener('click', () => {
+        page.innerText = `${page}`;
+        store.push(page)
+        this.getMovies(20);
 
-    const page3 = document.createElement("p");
-    page3.innerText = "3";
-    page3.addEventListener("click", () => {
-      this.getMovies(20);
-    });
+      })
+    }
 
-    const page4 = document.createElement("p");
-    page4.innerText = "4";
-    page4.classList.add("nav-link", "pageNumber");
-    page4.setAttribute("data-target", "next-movie");
 
-    const page5 = document.createElement("p");
-    page5.innerText = "5";
-    page5.classList.add("nav-link", "pageNumber");
-    page5.setAttribute("data-target", "next-movie");
 
-    const page6 = document.createElement("p");
-    page6.innerText = "6";
-    page6.classList.add("nav-link", "pageNumber");
-    page6.setAttribute("data-target", "next-movie");
-
-    const page7 = document.createElement("p");
-    page7.innerText = "7";
-    page7.classList.add("nav-link", "pageNumber");
-    page7.setAttribute("data-target", "next-movie");
-
-    const page8 = document.createElement("p");
-    page8.innerText = "8";
-    page8.classList.add("nav-link", "pageNumber");
-    page8.setAttribute("data-target", "next-movie");
-
-    const page9 = document.createElement("p");
-    page9.innerText = "9";
-    page9.classList.add("nav-link", "pageNumber");
-    page9.setAttribute("data-target", "next-movie");
-
-    const page10 = document.createElement("p");
-    page10.innerText = "10";
-    page10.classList.add("nav-link", "pageNumber");
-    page10.setAttribute("data-target", "next-movie");
 
     body.appendChild(paginationMovie);
     paginationMovie.appendChild(paginationDiv);
     paginationDiv.appendChild(pagesContainer);
     pagesContainer.appendChild(previous);
+    pagesContainer.appendChild(page)
 
-    pagesContainer.appendChild(page1);
-    pagesContainer.appendChild(page2);
-    pagesContainer.appendChild(page3);
-    pagesContainer.appendChild(page4);
-    pagesContainer.appendChild(page5);
-    pagesContainer.appendChild(page6);
-    pagesContainer.appendChild(page7);
-    pagesContainer.appendChild(page8);
-    pagesContainer.appendChild(page9);
-    pagesContainer.appendChild(page10);
+
 
     pagesContainer.appendChild(next);
   }
