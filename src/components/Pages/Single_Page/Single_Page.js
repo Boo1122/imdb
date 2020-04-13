572b0d7e2169475b36dbd46211a5fa2409fe1962
mia4565 <58949995+mia4565@users.noreply.github.com>
2020-04-13 17:51:18 +0300

    single page wip

--- src/components/Pages/Single_Page/Single_Page.js
+++ src/components/Pages/Single_Page/Single_Page.js
@@ -6,19 +6,16 @@ export class SinglePage {
   }
 
   mainContent(movie) {
-    console.log(movie)
+    console.log(movie);
     const main = document.getElementById("single-movie-page");
-    //main.style.backgroundImage = 
-    //"url(./public/img/star_wars_rise.jpg)";
-    
+    //main.style.backgroundImage = "url(./public/img/star_wars_rise.jpg)";
+
     main.innerHTML = null;
     const contentDiv = document.createElement("div");
     contentDiv.id = "main-div";
-    
 
     const movieContent = document.createElement("div");
     movieContent.id = "movie-content";
-   
 
     const poster = document.createElement("div");
     const img = document.createElement("img");
@@ -35,22 +32,27 @@ export class SinglePage {
 
     const facts = document.createElement("div");
     facts.className = "movie-facts";
+
     const genres = document.createElement("span");
     genres.className = "movie-genres";
     genres.innerText = `Genre: ${movie.Genre}`;
 
     const posterInfo = document.createElement("div");
     posterInfo.className = "poster-info";
+    const year = document.createElement("p");
+    year.innerText = `Year: ${movie.Year}`;
+    const language = document.createElement("p");
+    language.innerText = `Language: ${movie.Language}`;
     const country = document.createElement("p");
     country.className = "country";
     country.innerText = `Country: ${movie.Country}`;
-
-    const language = document.createElement("p");
-    language.innerText = `Language: ${movie.Language}`;
     const runtime = document.createElement("p");
     runtime.innerText = `Runtime: ${movie.Runtime}`;
-    const year = document.createElement("p");
-    year.innerText = `Year: ${movie.Year}`;
+
+    /*const iMDBratings = document.createElement("div");
+    iMDBratings.className = "imdb-ratings";
+    iMDBratings.id = "imdb-ratings-container";*/
+
     const imdbRating = document.createElement("p");
     imdbRating.innerText = `IMDb Rating: ${movie.imdbRating}`;
     const imdbVotes = document.createElement("p");
@@ -58,6 +60,7 @@ export class SinglePage {
 
     main.appendChild(contentDiv);
     contentDiv.appendChild(movieContent);
+    //movieDetails.appendChild(iMDBratings);
 
     movieContent.appendChild(poster);
     poster.appendChild(img);
@@ -70,13 +73,15 @@ export class SinglePage {
 
     titleDiv.appendChild(posterInfo);
     posterInfo.appendChild(year);
-
-    posterInfo.appendChild(imdbRating);
-    posterInfo.appendChild(imdbVotes);
-
     posterInfo.appendChild(language);
     posterInfo.appendChild(country);
     posterInfo.appendChild(runtime);
+
+    /*iMDBratings.appendChild(imdbRating);
+    iMDBratings.appendChild(imdbVotes);*/
+
+    posterInfo.appendChild(imdbRating);
+    posterInfo.appendChild(imdbVotes);
   }
 
   homeContainerBox() {
@@ -93,8 +98,8 @@ export class SinglePage {
     if (movieId) {
       console.log(movieId);
       fetch(`https://movies-api-siit.herokuapp.com/movies/${movieId}`)
-        .then((response) => response.json())
-        .then((json) => {
+        .then(response => response.json())
+        .then(json => {
           this.mainContent(json);
         });
     }
