514d1153029ba12542c684a45fb0a0db5c78af9e
Boo1122 <you@example.com>
2020-04-16 11:45:19 +0300

    input and register emptied

--- src/components/Pages/Login_Page/Login_Page.js
+++ src/components/Pages/Login_Page/Login_Page.js
@@ -79,25 +79,31 @@ export class LoginPage {
 
     logButton.addEventListener("click", (event) => {
       fetch("https://movies-api-siit.herokuapp.com/auth/login", {
-        method: "POST", // *GET, POST, PUT, DELETE, etc.
-        mode: "cors", // no-cors, *cors, same-origin
-        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-        credentials: "same-origin", // include, *same-origin, omit
-        headers: {
-          "Content-Type": "application/json",
-        },
-        redirect: "follow", // manual, *follow, error
-        referrerPolicy: "no-referrer", // no-referrer, *client
-        body: JSON.stringify({
-          username: `${userName.value}`,
-          password: `${passWord.value}`,
-        }),
-      })
+          method: "POST", // *GET, POST, PUT, DELETE, etc.
+          mode: "cors", // no-cors, *cors, same-origin
+          cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+          credentials: "same-origin", // include, *same-origin, omit
+          headers: {
+            "Content-Type": "application/json",
+          },
+          redirect: "follow", // manual, *follow, error
+          referrerPolicy: "no-referrer", // no-referrer, *client
+          body: JSON.stringify({
+            username: `${userName.value}`,
+            password: `${passWord.value}`,
+          }),
+        })
         .then((response) => response.json())
         .then((json) => {
           console.log(json);
           document.cookie = `token=${json.accessToken}`;
 
+
+          let eraseInputUser = document.getElementById("username-input-login")
+          eraseInputUser.value = '';
+
+          let eraseInputPass = document.getElementById("password-input-login")
+          eraseInputPass.value = '';
           this.handleButtonTransferLogin();
         });
     });
