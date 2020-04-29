a71b31cb280f5ded7c42b7c2d19360397d1ebbc7
mia4565 <58949995+mia4565@users.noreply.github.com>
2020-04-29 16:23:18 +0300

    fixed single page

--- src/components/Pages/USER_Logged_In/User_Utils.js
+++ src/components/Pages/USER_Logged_In/User_Utils.js
@@ -17,14 +17,20 @@ export function addMovieToAPI() {
       credentials: "same-origin", // include, *same-origin, omit
       headers: {
         "Content-Type": "application/json",
-        "X-Auth-Token": token,
+        "X-Auth-Token": token
       },
       redirect: "follow", // manual, *follow, error
       referrerPolicy: "no-referrer", // no-referrer, *client
-      body: JSON.stringify({ Title: title.value, Year: year.value, Runtime: runtime.value, Genre: genre.value, Language: language.value, Country: country.value, Poster: poster.value, imdbRating: imdbRating.value, imdbVotes: imdbVotes.value, imdbID: imdbID.value, Type: type.value}),
+      body: JSON.stringify({
+        Title: title.value,
+        Year: year.value,
+        ImdbID: imdbID.value,
+        Type: type.value,
+        Poster: posterUrl.value
+      })
     })
-      .then((response) => response.json())
-      .then((json) => {
+      .then(response => response.json())
+      .then(json => {
         console.log(json);
 
         if (json.accessToken) {
