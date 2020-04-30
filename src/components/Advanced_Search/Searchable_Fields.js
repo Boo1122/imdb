51f687f3f43d962387403c6dea317fc42c3b6d8a
George4888 <DanGeorge4888@gmail.com>
2020-04-30 20:29:49 +0300

    not working

--- src/components/Advanced_Search/Searchable_Fields.js
+++ src/components/Advanced_Search/Searchable_Fields.js
@@ -1,16 +1,16 @@
 function generateURL() {
   const title = document.getElementById("titleInput");
   const year = document.getElementById("yearInput");
-  const runtimeInput = document.getElementById("runtimeInput");
+  const runtime = document.getElementById("runtimeInput");
   const action = document.getElementById("action");
   const animation = document.getElementById("animation");
   const comedy = document.getElementById("comedy");
   const horror = document.getElementById("horror");
   const sciFi = document.getElementById("sciFi");
-  const languageInput = document.getElementById("languageInput");
-  const countryInput = document.getElementById("countryInput");
-  const imdbRatingInput = document.getElementById("imdbRatingInput");
-  const imdbVotesInput = document.getElementById("imdbVotesInput");
+  const language = document.getElementById("languageInput");
+  const country = document.getElementById("countryInput");
+  const imdbRating = document.getElementById("imdbRatingInput");
+  const imdbVotes = document.getElementById("imdbVotesInput");
   const imdbIDInput = document.getElementById("imdbIDInput");
 
   const movieInput = document.getElementById("movieInput");
@@ -18,7 +18,9 @@ function generateURL() {
 
   const lastSearch = document.getElementById("last-search");
 
-  const url = `https://movies-app-siit.herokuapp.com/movies?Title=${title.value}&Year=${year.value}`;
+  // const url = `https://movies-app-siit.herokuapp.com/movies?Title=${title.value}&Year=${year.value}&Runtime=${runtime.value}&Language=${language.value}&Country=${country.value}&imdbRating=${imdbRating.value}`;
+
+  const url = `https://movies-app-siit.herokuapp.com/movies?Year=${year.value}&imdbRating=${imdbRating.value}`;
 
   return url;
 }
