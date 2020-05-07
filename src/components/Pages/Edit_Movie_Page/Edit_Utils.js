5f69f0cc80d099ab12cf967c5be40cb19476399d
George4888 <DanGeorge4888@gmail.com>
2020-05-07 11:07:12 +0300

    id changed for button click

--- src/components/Pages/Edit_Movie_Page/Edit_Utils.js
+++ src/components/Pages/Edit_Movie_Page/Edit_Utils.js
@@ -1,40 +1,44 @@
 import Cookie from "js-cookie";
 
 export function editMovieToAPI() {
-  document.getElementById("editMovieButton").addEventListener("click", () => {
-    const title = document.getElementById("edit-movie-title");
-    const year = document.getElementById("edit-movie-year");
-    const imdbID = document.getElementById("edit-movie-ImdbID");
-    const type = document.getElementById("edit-movie-type");
-    const posterUrl = document.getElementById("edit-movie-poster");
+  document
+    .getElementById("editMovieDetailsButton")
+    .addEventListener("click", () => {
+      const title = document.getElementById("edit-movie-title");
+      const year = document.getElementById("edit-movie-year");
+      const imdbID = document.getElementById("edit-movie-ImdbID");
+      const type = document.getElementById("edit-movie-type");
+      const posterUrl = document.getElementById("edit-movie-poster");
 
-    const token = Cookie.get("token");
-    let url = `https://movies-app-siit.herokuapp.com/movies${/*this.*/movie.id}`;
+      const token = Cookie.get("token");
+      let url = `https://movies-app-siit.herokuapp.com/movies${
+        /*this.*/ movie.id
+      }`;
 
-    fetch(url, {
-      method: "PUT", // *GET, POST, PUT, DELETE, etc.
-      mode: "cors", // no-cors, *cors, same-origin
-      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
-      credentials: "same-origin", // include, *same-origin, omit
-      headers: {
-        "Content-Type": "application/json",
-        "X-Auth-Token": token
-      },
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *client
-      body: JSON.stringify({
-        Title: `${title.value}`,
-        Year: `${year.value}`,
-        ImdbID: `${imdbID.value}`,
-        Type: `${type.value}`,
-        Poster: `${posterUrl.value}`
+      fetch(url, {
+        method: "PUT", // *GET, POST, PUT, DELETE, etc.
+        mode: "cors", // no-cors, *cors, same-origin
+        cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/json",
+          "X-Auth-Token": token,
+        },
+        redirect: "follow", // manual, *follow, error
+        referrerPolicy: "no-referrer", // no-referrer, *client
+        body: JSON.stringify({
+          Title: `${title.value}`,
+          Year: `${year.value}`,
+          ImdbID: `${imdbID.value}`,
+          Type: `${type.value}`,
+          Poster: `${posterUrl.value}`,
+        }),
       })
-    })
-      .then(response => response.json())
-      .then(json => {
-        console.log(json);
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json);
 
-        /*if (json.accessToken) {
+          /*if (json.accessToken) {
           let emptyTitleInput = document.getElementById("edit-movie-title");
           emptyTitleInput.value = "";
 
@@ -50,6 +54,6 @@ export function editMovieToAPI() {
           let emptyPosterInput = document.getElementById("edit-movie-poster");
           emptyPosterInput.value = "";
         }*/
-      });
-  });
+        });
+    });
 }
