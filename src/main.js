d5ca0492b2c41f308db8afdadc1f39d71f8535fc
George4888 <DanGeorge4888@gmail.com>
2020-05-10 16:02:42 +0300

    k

--- src/main.js
+++ src/main.js
@@ -1,43 +1,17 @@
-import {
-    Header
-} from "./components/Home_Page_Header/Home_Page_Header";
-import {
-    RegisterPage
-} from "./components/Pages/Register_Page/Register_Page";
-import {
-    LoginPage
-} from "./components/Pages/Login_Page/Login_Page";
-import {
-    HomePage
-} from "./components/Pages/Home_Page/Home_Page";
+import { Header } from "./components/Home_Page_Header/Home_Page_Header";
+import { RegisterPage } from "./components/Pages/Register_Page/Register_Page";
+import { LoginPage } from "./components/Pages/Login_Page/Login_Page";
+import { HomePage } from "./components/Pages/Home_Page/Home_Page";
 import "./main.css";
-import {
-    Footer
-} from "./components/Footer/Footer";
-import {
-    navigate
-} from "./components/Navigate_History/Navigate_History";
-import {
-    SinglePage
-} from "./components/Pages/Single_Page/Single_Page";
-import {
-    MoviePage
-} from "./components/Pages/Movie_Page/Movie_Page";
-import {
-    LogOutPage
-} from "./components/Pages/LogOut_Page/LogOut_Page";
-import {
-    AdvancedSearch
-} from "./components/Advanced_Search/Advanced_Search";
-import {
-    switchButtons
-} from "./components/Pages/USER_Logged_In/Switch_Buttones";
-import {
-    UserOptions
-} from "./components/Pages/USER_Logged_In/User_Options";
-import {
-    EditMoviePage
-} from "./components/Pages/Edit_Movie_Page/Edit_Movie_Page";
+import { Footer } from "./components/Footer/Footer";
+import { navigate } from "./components/Navigate_History/Navigate_History";
+import { SinglePage } from "./components/Pages/Single_Page/Single_Page";
+import { MoviePage } from "./components/Pages/Movie_Page/Movie_Page";
+import { LogOutPage } from "./components/Pages/LogOut_Page/LogOut_Page";
+import { AdvancedSearch } from "./components/Advanced_Search/Advanced_Search";
+import { switchButtons } from "./components/Pages/USER_Logged_In/Switch_Buttones";
+import { UserOptions } from "./components/Pages/USER_Logged_In/User_Options";
+import { EditMoviePage } from "./components/Pages/Edit_Movie_Page/Edit_Movie_Page";
 
 new Header();
 new HomePage();
